9f1ce09a4a20549f62b7af15f126c015fb60dee1
gopalojha <gopal.ojha@gmail.com>
2015-08-21 11:32:16 -0500

    Create server.R

---
+++ server.R
@@ -0,0 +1,21 @@
+library(shiny)
+library(UsingR); data(galton);
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+  # Expression that generates a histogram. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should re-execute automatically
+  #     when inputs change
+  #  2) Its output type is a plot
+
+  output$distPlot <- renderPlot({
+    x    <- galton$child  # Galton's Child Data
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'skyblue', border = 'white')
+  })
+})
